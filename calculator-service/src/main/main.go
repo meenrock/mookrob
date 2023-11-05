233dc309d2e183bc7fc18d2d3d4aceadd3428f48
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2023-11-05 23:11:04 +0700

    add rabbitmq

--- calculator-service/src/main/main.go
+++ calculator-service/src/main/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 
-	mqtt_services "github.com/mookrob/servicecalculator/main/services/mq"
+	repositories "github.com/mookrob/servicecalculator/main/repositories"
+	"github.com/mookrob/servicecalculator/main/routers"
+
+	// mqtt_services "github.com/mookrob/servicecalculator/main/services/mq"
+	rest_services "github.com/mookrob/servicecalculator/main/services/rest"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -12,7 +17,8 @@ import (
 )
 
 func main() {
-	r := gin.Default()
+	gin_engine := gin.Default()
+	gin_engine.Use(gin.Recovery())
 
 	viper.SetConfigName("config") // Name of the config file (without extension)
 	viper.SetConfigType("yaml")   // Type of the config file (yaml, json, etc.)
@@ -24,37 +30,38 @@ func main() {
 	}
 
 	// DB connection
-	// DB_HOST := viper.GetString("database.host")
-	// DB_PORT := viper.GetString("database.port")
-	// DB_NAME := viper.GetString("database.name")
-	// DB_USER := viper.GetString("database.user")
-	// DB_PASSWORD := viper.GetString("database.password")
-	PORT := viper.GetString("server.port")
+	DB_HOST := viper.GetString("database.host")
+	DB_PORT := viper.GetString("database.port")
+	DB_NAME := viper.GetString("database.name")
+	DB_USER := viper.GetString("database.user")
+	DB_PASSWORD := viper.GetString("database.password")
+	PORT := viper.GetString("server.rest-port")
 
-	// // connect postgres
-	// psqlInfo := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
-	// db, err := sql.Open("pgx", psqlInfo)
-	// if err != nil {
-	// 	log.Fatalf("Error while reading config file %s", err)
-	// }
+	// connect postgres
+	psqlInfo := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+	db, err := sql.Open("pgx", psqlInfo)
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
 
 	// create instances of services and controllers
-	// userRepository := repositories.NewUserRepository(db)
+	calculatorRepository := repositories.NewUserCalculatorRepository(db)
 	// userService := services.NewUserService(userRepository)
 	// routers.SetUserRoutes(r, userService)
 
-	// Start the server
-	port := fmt.Sprintf(":%v", PORT)
-	fmt.Println("Server Running on Port", port)
-	if err := r.Run(port); err != nil {
-		log.Fatal(err)
-	}
-
 	go func() {
-		conn, ch := mqtt_services.CreateRabbitMQConnection()
-		defer conn.Close()
-		defer ch.Close()
+		// Start the server
 
+		userCalculatorService := rest_services.NewUserCalculatorRestService(calculatorRepository)
+		routers.SetUserCalculatorRoutes(gin_engine, userCalculatorService)
+
+		rest_port := fmt.Sprintf(":%v", PORT)
+		fmt.Println("Server Running on Port", rest_port)
+		if err := gin_engine.Run(rest_port); err != nil {
+			log.Fatal(err)
+		}
 	}()
+
+	select {}
 
 }
