8ae96385c3b5a7005487e32206d64bdfa4350020
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2023-11-18 22:31:11 +0700

    update calculator and user grpc

--- calculator-service/src/main/routers/calculator_router.go
+++ calculator-service/src/main/routers/calculator_router.go
@@ -46,33 +46,3 @@ func (r *Router) handleSaveUserBodyData(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "User data saved successfully"})
 }
-
-// import (
-// 	"net/http"
-
-// 	mqtt "github.com/mookrob/servicecalculator/main/services/mq"
-// 	services "github.com/mookrob/servicecalculator/main/services/rest"
-
-// 	rabbitmq "github.com/mookrob/servicecalculator/main/rabbitmq"
-
-// 	"github.com/gin-gonic/gin"
-// )
-
-// type Router struct {
-// 	*http.ServeMux
-// }
-
-// func NewRouter() *Router {
-// 	return &Router{http.NewServeMux()}
-// }
-
-// func (rx *Router) SetUserCalculatorRoutes(router *gin.Engine, ctrls *services.UserCalculatorService, rabbitmq *rabbitmq.RabbitMQ) {
-// 	createparameter := mqtt.NewCreate(rabbitmq)
-// 	rx.HandleFunc("/parameter/add", createparameter.Handle)
-
-// 	calRoutes := router.Group("/api/calculator")
-// 	{
-// 		calRoutes.GET("/test", ctrls.GetUserCalculationByUserId)
-// 	}
-
-// }
